46d23de17fcaf156cc018acc43f751d8561a0cad
Sukdeb Sahu <86526080+Sukdeb223@users.noreply.github.com>
2023-03-05 11:05:09 +0530

    user input added

--- Java_code/HelloWrold.java
+++ Java_code/HelloWrold.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class HelloWrold {
       public static void main(String[] args) {
+            Scanner input =new Scanner(System.in);
             System.out.println("Hello Wrold 2");
             System.out.println("This is 2nd java File");
       }
---
+++ Java_code/UserInput.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class UserInput {
+      public static void main(String[] args) {
+      Scanner input=new Scanner(System.in);   
+      Scanner input1=new Scanner(System.in);   
+      System.out.println("Enter any Number:");   
+
+      //for int Data Type...
+      int num=input.nextInt();//taking user input using nextint function...
+      System.out.println("int :"+num);
+
+      //float 
+      System.out.println("Enter Any Float Value");
+      float num1=input.nextFloat();
+      System.out.println("Float"+num1);
+
+      //double
+      System.out.println("Enter Any Double Value");
+      double num2=input.nextDouble();
+      System.out.println("double"+num2);
+
+      //String
+      System.out.println("Enter Any String Value");
+      String name=input1.nextLine();
+      System.out.println("String "+ name);
+      }
+}
